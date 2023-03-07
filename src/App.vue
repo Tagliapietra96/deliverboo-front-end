efdf4842ea705187dfbcb64c85b92872e6599432
LuigginaRossi <luigginavaldivia@gmail.com>
2023-03-07 16:02:24 +0100

    scss changes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template lang="">
   <router-view></router-view>
+  <DishesList></DishesList>
 </template>
 
 <script>
 import { store } from "./stores/store";
+import DishesList from "./components/dishes/DishesList.vue";
 export default {
+  components: {DishesList},
   data() {
     return {};
   },
@@ -13,5 +16,5 @@ export default {
 </script>
 
 <style lang="scss">
-
+@use "./styles/main.scss";
 </style>
