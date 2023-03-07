d7a94dfafad5802524ec1f4c71c6fb58771ee342
LuigginaRossi <luigginavaldivia@gmail.com>
2023-03-07 14:48:07 +0100

    added NotFound page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,15 @@
+<template lang="">
+
+
+</template>
+  
+<script>
+import { store } from '../stores/store';
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
