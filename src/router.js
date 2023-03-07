d32cd09e911492a5accf11eb8da4c60b1fd14725
LuigginaRossi <luigginavaldivia@gmail.com>
2023-03-07 14:47:44 +0100

    added NotFound page

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import WelcomePage from "./pages/Welcome.vue";
 import RestaurantsIndexPage from "./pages/restaurants/Index.vue";
 import RestautantShowPage from "./pages/restaurants/Show.vue";
+import NotFoundPage from "./pages/NotFound.vue";
 
 const router = createRouter({
   // Serve ad indicare come gestire l'url al cambio pagina
@@ -27,6 +28,11 @@ const router = createRouter({
       name: "dish",
       component: RestautantShowPage,
     },
+    {
+      path: "/:catchAll(.*)",
+      name: "not-found",
+      component: NotFoundPage
+    }
   ],
 });
 
