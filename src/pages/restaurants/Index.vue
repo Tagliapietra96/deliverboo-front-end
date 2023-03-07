7edfa795ca5ceabe747ceaf48511047528a8d9d8
PaoloWD <paolo.lore.93@gmail.com>
2023-03-07 18:23:40 +0100

    add filter

--- src/pages/restaurants/Index.vue
+++ src/pages/restaurants/Index.vue
@@ -1,21 +1,51 @@
-<template lang="">
-  <div v-for="element in store.dt.restaurantsList">
-    {{ element }}
+<template>
+  <div>
+    <div v-for="category in store.dt.categoriesList" :key="category.id">
+      <input
+        type="checkbox"
+        v-model="selectedCategory"
+        :value="category.name"
+      />
+      <label>{{ category.name }}</label>
+    </div>
+    <button @click="fetchRestaurants">Cerca</button>
+    <div v-for="(restaurant, i) in store.dt.restaurantsList" :key="i">
+      {{ restaurant.name }}
+    </div>
   </div>
 </template>
 
 <script>
 import { store } from "../../stores/store";
+import axios from "axios";
 export default {
   data() {
     return {
       store,
+      selectedCategory: [], // aggiungi la proprietà selectedCategory all'oggetto data
     };
   },
+  methods: {
+    fetchRestaurants() {
+      axios
+        .get(store.dt.beUrl + store.dt.restaurantsUrl, {
+          params: {
+            category: this.selectedCategory,
+          },
+        })
+        .then((response) => {
+          this.store.dt.restaurantsList = response.data; // aggiorna la lista dei ristoranti con la risposta della chiamata
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
   mounted() {
+    store.fn.fetchCategories();
     store.fn.fetchRestaurants();
   },
 };
 </script>
 
-<style lang="scss"></style>
+<style scoped></style>
--- src/stores/store.js
+++ src/stores/store.js
@@ -1,51 +1,51 @@
-import { reactive } from 'vue';
-import { api } from './api';
+import { reactive } from "vue";
+import { api } from "./api";
 export const store = reactive({
-    dt:{
-        headerLinks: [
-            {
-                name: 'Lavora con noi',
-                url: 'http://127.0.0.1:8000',
-                active: false
-            },
-            {
-                name: 'Vai ai ristoranti',
-                url: '#',
-                active: false
-            },
-            {
-                name: 'Il tuo carrello',
-                url: '#',
-                active: false
-            },
-        ],
-        loading: false,
-        beUrl: 'http://127.0.0.1:8000',
-        categoriesUrl: '/api/categories',
-        restaurantsUrl: '/api/restaurants',
-        dishesUrl: '/api/dishes',
-        categoriesList: [],
-        restaurantsList: [],
-        dishesList: [],
+  dt: {
+    headerLinks: [
+      {
+        name: "Lavora con noi",
+        url: "http://127.0.0.1:8000",
+        active: false,
+      },
+      {
+        name: "Vai ai ristoranti",
+        url: "#",
+        active: false,
+      },
+      {
+        name: "Il tuo carrello",
+        url: "#",
+        active: false,
+      },
+    ],
+    loading: false,
+    beUrl: "http://127.0.0.1:8000",
+    categoriesUrl: "/api/categories",
+    restaurantsUrl: "/api/restaurants",
+    dishesUrl: "/api/dishes",
+    categoriesList: [],
+    restaurantsList: [],
+    dishesList: [],
+  },
+  fn: {
+    fetchCategories() {
+      api.get.index.categories();
     },
-    fn:{
-        fetchCategories(){
-            api.get.index.categories();
-        },
-        fetchRestaurants(){
-            api.get.index.restaurants();
-        },
-        fetchDishes(){
-            api.get.index.dishes();
-        },
-        showCategories(){
-            api.get.show.categories();
-        },
-        showRestaurants(){
-            api.get.show.restaurants();
-        },
-        showDishes(){
-            api.get.show.dishes();
-        }
-    }
+    fetchRestaurants() {
+      api.get.index.restaurants();
+    },
+    fetchDishes() {
+      api.get.index.dishes();
+    },
+    showCategories() {
+      api.get.show.categories();
+    },
+    showRestaurants() {
+      api.get.show.restaurants();
+    },
+    showDishes() {
+      api.get.show.dishes();
+    },
+  },
 });
