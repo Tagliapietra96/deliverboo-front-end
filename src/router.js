f768485ca07f4e503be95e1e154e4be50ef23c49
PaoloWD <paolo.lore.93@gmail.com>
2023-03-07 13:05:24 +0100

    fix router

--- src/router.js
+++ src/router.js
@@ -1,29 +1,28 @@
 import { createRouter, createWebHistory } from "vue-router";
-import WelcomePage from './pages/WelcomePage.vue';
-import RestaurantsIndex from './pages/restaurants/Index.vue';
-import RestautantShow from './pages/restaurants/Show.vue';
+import WelcomePage from "./pages/Welcome.vue";
+import RestaurantsIndex from "./pages/restaurants/Index.vue";
+import RestautantShow from "./pages/restaurants/Show.vue";
 
 const router = createRouter({
-    // Serve ad indicare come gestire l'url al cambio pagina
-    history: createWebHistory(),
-    routes: [
-      {
-        path: "/",
-        name: "welcome",
-        component: WelcomePage,
-      },
-       {
-         path: "/i-nostri-ristoranti",
-         name: "restaurants",
-         component: RestaurantsIndex,
-       },
-        {
-         path: "/ristorante",
-         name: "restaurant",
-         component: RestautantShow,
-        },
-      
-    ]
-  });
-  
-  export { router };
+  // Serve ad indicare come gestire l'url al cambio pagina
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "welcome",
+      component: WelcomePage,
+    },
+    {
+      path: "/i-nostri-ristoranti",
+      name: "restaurants",
+      component: RestaurantsIndex,
+    },
+    {
+      path: "/ristorante",
+      name: "restaurant",
+      component: RestautantShow,
+    },
+  ],
+});
+
+export { router };
