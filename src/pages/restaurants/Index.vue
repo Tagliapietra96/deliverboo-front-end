3437d87e6c3d97d2439c69e2eaf875ffadded621
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2023-03-08 14:05:19 +0100

    inizio grafica ristoranti

--- src/pages/restaurants/Index.vue
+++ src/pages/restaurants/Index.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <div
-      v-for="category in store.dt.categoriesList"
+  <div class="container">
+  <div class="d-flex">
+    <div class="link-container">
+      <span class="title pt-3 ps-3">Categoria</span>
+    <div class="pt-3 px-3" v-for="category in store.dt.categoriesList"
       :key="category.id"
       @click="store.fn.fetchRestaurants(category.name)"
     >
@@ -9,14 +11,23 @@
         type="checkbox"
         :checked="store.dt.selectedCategories.includes(category.name)"
       />
-      <label>{{ category.name }}</label>
+      <label class="ps-2">{{ category.name }}</label>
     </div>
-    <div v-for="(restaurant, i) in store.dt.restaurantsList" :key="i">
-      <div v-if="restaurant">
-        {{ restaurant.name }}
-        <button @click="fetchDishes(restaurant.id)">Visualizza piatti</button>
-      </div>
+  </div>
+  <div class="">
+    <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 justify-content-center">
+        <div class=" col card m-3 shadow p-3 rounded-3" v-for="(restaurant, i) in store.dt.restaurantsList" :key="i">
+          <div v-if="restaurant">
+            <div class="img-container">
+              <img class="my-img-fluid rounded-3" :src="restaurant.image" alt="">
+            </div>
+            <h2 class="title"> {{ restaurant.name }} </h2>
+          <button @click="fetchDishes(restaurant.id)">Menù</button>
+          </div>
+        </div>
     </div>
+</div>
+</div>    
     <div v-for="(dish, i) in store.dt.dishesList" :key="i">
       <div v-if="dish">
         {{ dish.name }}
@@ -55,4 +66,31 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.link-container {
+  width: 300px;
+
+} 
+
+.title{
+  font-family: bold;
+  font-weight: bold;
+  font-size: 1.5rem;
+  text-align: center;
+  padding-bottom: 1rem;
+}
+
+.img-container {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  padding-bottom: 1rem;
+}
+
+.my-img-fluid {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+</style>
