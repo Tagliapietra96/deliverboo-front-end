5378d6e34c553f6854bb355475eff8babd7f2b44
Tagliapietra96 <tagliatek96@gmail.com>
2023-03-09 17:22:20 +0100

    fixed submit on searchbar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row justify-content-center py-5">
             <div class="col-12 col-md-6 my-border rounded-5">
-                <form class="form-inline d-flex">
+                <form @submit.prevent="" class="form-inline d-flex">
                     <input class="form-control mr-sm-2 border-0 rounded-5" type="search" placeholder="Cerca" aria-label="Search">
                     <button class="btn btn-custom px-md-5 py-md-2 rounded-5 my-2 my-sm-0 ms-3 " type="submit"><i class="fa-solid fa-magnifying-glass text-white"></i></button>
                 </form>
