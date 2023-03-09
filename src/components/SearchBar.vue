acdc434b7a98df757519bd19923195dfef5f61a6
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2023-03-09 16:47:25 +0100

    add box-shadow none

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center py-5 ">
-            <div class="col-6 my-border rounded-5">
-                <form class="form-inline d-flex ">
-                    <input class="form-control mr-sm-2 border-0" type="search" placeholder="Cerca" aria-label="Search">
-                    <button class="btn btn-custom px-5 py-2 rounded-5 my-2 my-sm-0 ms-3 " type="submit"><i class="fa-solid fa-magnifying-glass text-white"></i></button>
+        <div class="row justify-content-center py-5">
+            <div class="col-12 col-md-6 my-border rounded-5">
+                <form class="form-inline d-flex">
+                    <input class="form-control mr-sm-2 border-0 rounded-5" type="search" placeholder="Cerca" aria-label="Search">
+                    <button class="btn btn-custom px-md-5 py-md-2 rounded-5 my-2 my-sm-0 ms-3 " type="submit"><i class="fa-solid fa-magnifying-glass text-white"></i></button>
                 </form>
             </div>
         </div>
@@ -28,7 +28,11 @@
   
   .my-border{
     border: 1px solid #c76262;
-    padding: .4rem ;
+    padding: .2rem ;
+  }
+
+  .form-control:focus{
+    box-shadow: none !important;
   }
 
   </style>
