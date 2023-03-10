6177bad118766c037cad165744c0f6f0a2887709
PaoloWD <paolo.lore.93@gmail.com>
2023-03-10 16:27:59 +0100

    test

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -5,19 +5,27 @@
     <div class="row row-cols-1">
       <div class="col" v-for="(item, index) in store.dt.myChart" :key="index">
         <div class="single-row py-3 px-5">
-            <div class="fw-bolder">{{ item.item.name }}:</div>
+          <div class="fw-bolder">{{ item.item.name }}:</div>
           <div>€ {{ item.price }}</div>
         </div>
       </div>
     </div>
-    <button v-if="store.dt.myChart.length > 0" class="btn btn-ptimary btn-custom mt-3" @click="dropChart()">
+    <button
+      v-if="store.dt.myChart.length > 0"
+      class="btn btn-ptimary btn-custom mt-3"
+      @click="dropChart()"
+    >
       Elimina carrello
     </button>
   </div>
+  <form action="" method="POST">
+    <button type="submit" @click.prevent="payment()">paga</button>
+  </form>
 </template>
 
 <script>
-import { store } from '../stores/store';
+import { store } from "../stores/store";
+import axios from "axios";
 export default {
   data() {
     return {
@@ -25,17 +33,32 @@ export default {
     };
   },
   methods: {
-    dropChart(){
+    dropChart() {
       store.dt.myChart = [];
       store.fn.saveStorage();
       store.fn.loadStorage();
     },
+    payment() {
+      console.log("ciao");
+      axios
+        .post(
+          "http://127.0.0.1:8000/api/orders/make/payment?token=fake-valid-nonce&dish=5&dish=9"
+        )
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+          store.dt.loading = false;
+        });
+    },
   },
+
   mounted() {
     store.fn.loadStorage();
   },
   beforeUnmount() {
-    store.fn.saveStorage()
+    store.fn.saveStorage();
   },
 };
 </script>
