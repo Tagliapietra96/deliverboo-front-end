62f23f4812ab2a23d96ae3a2f3ef1b05ea57605d
Tagliapietra96 <tagliatek96@gmail.com>
2023-03-14 16:16:43 +0100

    vi prego non guardate ne toccate il codice della pagina cart

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,5 +1,62 @@
 <template>
   <Loader v-if="store.dt.loading" />
+
+  <div v-if="selectedDish !== -1 && !forceExit" :class="selectedDish !== -1 ? 'active' : ''" class="selected-dish">
+    <div class="h-100 w-100 bg-invisible">
+      <div class="card card-show border-0 rounded-4 overflow-hidden">
+        <button @click="selectedDish = -1" class="btn btn-primary btn-custom close-show">
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+        <img v-if="store.dt.myChart[selectedDish].item.image.includes('http')"
+          :src="store.dt.myChart[selectedDish].item.image" class="card-img-top" alt="..."
+          :style="(store.dt.myChart[selectedDish].item.visibility) ? 'filter: grayscale(0)' : 'filter: grayscale(100%)'" />
+        <img v-else :src="store.dt.beUrl + '/storage/' + store.dt.myChart[selectedDish].item.image" class="card-img-top"
+          alt="..."
+          :style="(store.dt.myChart[selectedDish].item.visibility) ? 'filter: grayscale(0)' : 'filter: grayscale(100%)'" />
+        <div class="card-body">
+          <h5 class="card-title">{{ store.dt.myChart[selectedDish].item.name }}</h5>
+          <p class="card-text">{{ store.dt.myChart[selectedDish].item.description }}</p>
+          <div class="px-3">
+            <div class="bg-light border text-center py-3 fs-4 rounded-2">
+              € {{ totalPrice(store.dt.myChart[selectedDish].item.price, store.dt.myChart[selectedDish].quantity) }}
+            </div>
+            
+            <div class="text-center py-3" v-if="store.dt.myChart[selectedDish].item.visibility">
+              <div class="d-flex justify-content-around pt-3">
+                <button class="btn btn-primary btn-custom btn-sm px-3"
+                  @click="(store.dt.myChart[selectedDish].quantity === 1) ? store.dt.myChart[selectedDish].quantity = 99 : store.dt.myChart[selectedDish].quantity--; store.dt.myChart[selectedDish].price = totalPrice(store.dt.myChart[selectedDish].item.price, store.dt.myChart[selectedDish].quantity); store.fn.saveStorage(); store.fn.loadStorage()">
+                  -
+                </button>
+                <div class="card w-25 text-center py-2 d-none d-lg-block">
+                  {{ store.dt.myChart[selectedDish].quantity }}
+                </div>
+                <button class="btn btn-primary btn-custom btn-sm px-3"
+                  @click="(store.dt.myChart[selectedDish].quantity === 99) ? store.dt.myChart[selectedDish].quantity = 1 : store.dt.myChart[selectedDish].quantity++; store.dt.myChart[selectedDish].price = totalPrice(store.dt.myChart[selectedDish].item.price, store.dt.myChart[selectedDish].quantity); store.fn.saveStorage(); store.fn.loadStorage()">
+                  +
+                </button>
+              </div>
+
+              <div class="text-center pt-3">
+                <button @click="forceExit = true; store.dt.myChart.splice(selectedDish, 1); selectedDish = -1; store.fn.saveStorage(); store.fn.loadStorage()" class="delete btn-sm btn btn-primary btn-custom">
+              <i class="fa-solid fa-trash-can"></i>
+            </button>
+              </div>
+            </div>
+            <div class="pt-3" v-else>
+              <h5 class="text-center custom-color fw-bold">Prodotto attualmente non disponibile</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
   <div class="container">
     <h2 class="pt-3 pb-2">Il tuo Carrello</h2>
     <div class="py-1 mt-2 mb-5 custom-bg w-100 rounded-1"></div>
@@ -8,26 +65,20 @@
         <div class="single-row py-3 px-5">
           <div class="row">
             <div class="col-2 col-lg-1 align-items-center d-flex">
-              <div
-                class="card d-flex justify-content-center align-items-center fw-bolder"
-                style="aspect-ratio: 1/1; width: 50px"
-              >
+              <div class="card d-flex justify-content-center align-items-center fw-bolder"
+                style="aspect-ratio: 1/1; width: 50px">
                 {{ item.quantity }}
               </div>
             </div>
             <div class="col-6 col-lg-7 d-flex align-items-center">
               <div class="fw-bolder">{{ item.item.name }}:</div>
             </div>
-            <div
-              class="col-3 text-end d-flex align-items-center justify-content-end"
-            >
+            <div class="col-3 text-end d-flex align-items-center justify-content-end">
               <div>€ {{ item.price }}</div>
             </div>
             <div class="col-1 align-items-center d-flex">
-              <button
-                class="btn btn-primary btn-custom"
-                style="aspect-ratio: 1/1; width: 50px"
-              >
+              <button class="btn btn-primary btn-custom" style="aspect-ratio: 1/1; width: 50px"
+                @click="selectedDish = index; forceExit = false">
                 <i class="fa-solid fa-pen"></i>
               </button>
             </div>
@@ -45,11 +96,7 @@
         </div>
       </div>
     </div>
-    <button
-      v-if="store.dt.myChart.length > 0"
-      class="btn btn-ptimary btn-custom mt-3"
-      @click="dropChart()"
-    >
+    <button v-if="store.dt.myChart.length > 0" class="btn btn-ptimary btn-custom mt-3" @click="dropChart()">
       Elimina carrello
     </button>
 
@@ -60,74 +107,34 @@
       <div class="col-5">
         <form class="row g-3" novalidate>
           <div class="col-md-12">
-            <label for="customer_name" class="form-label"
-              >Nome e Cognome*</label
-            >
-            <input
-              type="text"
-              v-model="customer_name"
-              class="form-control"
-              name="customer_name"
-              :class="{ 'is-invalid': formSubmitted && !customer_name }"
-              required
-            />
-            <div
-              class="invalid-feedback"
-              v-if="formSubmitted && !customer_name"
-            >
+            <label for="customer_name" class="form-label">Nome e Cognome*</label>
+            <input type="text" v-model="customer_name" class="form-control" name="customer_name"
+              :class="{ 'is-invalid': formSubmitted && !customer_name }" required />
+            <div class="invalid-feedback" v-if="formSubmitted && !customer_name">
               Inserisci nome e cognome.
             </div>
           </div>
           <div class="col-md-12">
             <label for="customer_address" class="form-label">Indirizzo*</label>
-            <input
-              type="text"
-              v-model="customer_address"
-              class="form-control"
-              name="customer_address"
-              :class="{ 'is-invalid': formSubmitted && !customer_address }"
-              required
-            />
-            <div
-              class="invalid-feedback"
-              v-if="formSubmitted && !customer_address"
-            >
+            <input type="text" v-model="customer_address" class="form-control" name="customer_address"
+              :class="{ 'is-invalid': formSubmitted && !customer_address }" required />
+            <div class="invalid-feedback" v-if="formSubmitted && !customer_address">
               Inserisci un indirizzo.
             </div>
           </div>
           <div class="col-md-12">
-            <label for="customer_phone" class="form-label"
-              >Numero di telefono*</label
-            >
-            <input
-              type="number"
-              v-model="customer_phone"
-              class="form-control"
-              name="customer_phone"
-              :class="{ 'is-invalid': formSubmitted && !customer_phone }"
-              required
-            />
-            <div
-              class="invalid-feedback"
-              v-if="formSubmitted && !customer_phone"
-            >
+            <label for="customer_phone" class="form-label">Numero di telefono*</label>
+            <input type="number" v-model="customer_phone" class="form-control" name="customer_phone"
+              :class="{ 'is-invalid': formSubmitted && !customer_phone }" required />
+            <div class="invalid-feedback" v-if="formSubmitted && !customer_phone">
               Inserisci un numero di telefono.
             </div>
           </div>
           <div class="col-md-12">
             <label for="customer_email" class="form-label">E-mail*</label>
-            <input
-              type="email"
-              v-model="customer_email"
-              class="form-control"
-              name="customer_email"
-              :class="{ 'is-invalid': formSubmitted && !customer_email }"
-              required
-            />
-            <div
-              class="invalid-feedback"
-              v-if="formSubmitted && !customer_email"
-            >
+            <input type="email" v-model="customer_email" class="form-control" name="customer_email"
+              :class="{ 'is-invalid': formSubmitted && !customer_email }" required />
+            <div class="invalid-feedback" v-if="formSubmitted && !customer_email">
               Inserisci una mail valida.
             </div>
           </div>
@@ -138,12 +145,7 @@
         <div id="dropin-wrapper">
           <div id="checkout-message"></div>
           <div id="dropin-container"></div>
-          <button
-            class="btn btn-primary btn-custom mb-5"
-            @click="submit"
-            :disabled="!formComplete"
-            id="submit-button"
-          >
+          <button class="btn btn-primary btn-custom mb-5" @click="submit" :disabled="!formComplete" id="submit-button">
             Submit payment
           </button>
         </div>
@@ -153,6 +155,7 @@
 </template>
 
 <script>
+
 import { router } from "../router";
 window.addEventListener("DOMContentLoaded", function () {
   var payButton = document.querySelector("#submit-button");
@@ -216,6 +219,8 @@ export default {
       formSubmitted: false,
       store,
       resultPayment: "",
+      selectedDish: -1,
+      forceExit: true,
     };
   },
   methods: {
@@ -223,6 +228,10 @@ export default {
       store.dt.myChart = [];
       store.fn.saveStorage();
       store.fn.loadStorage();
+    },
+    totalPrice(num1, num2) {
+      let total = num1 * num2;
+      return (Math.round(total * 100) / 100).toFixed(2);
     },
     submit() {
       store.dt.payLink = this.dishesBuy;
@@ -323,6 +332,61 @@ export default {
   .single-row {
     border-bottom: 1px solid #c76262;
     border-collapse: collapse !important;
+  }
+}
+
+.selected-dish {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba($color: #000000, $alpha: 0.7);
+  z-index: 9999999999999999999992;
+  opacity: 0;
+
+  &.active {
+    opacity: 1;
+  }
+
+  .card-show {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 40vw;
+
+    &:hover {
+      .close-show {
+        opacity: 1;
+      }
+    }
+
+    .close-show {
+      position: absolute;
+      top: 15px;
+      right: 15px;
+      height: 40px;
+      width: 40px;
+      border-radius: 50%;
+      opacity: 0.3;
+      transition: opacity, 0.4s;
+      z-index: 9999999999999999999999;
+
+      &:hover {
+        transform: rotate(360deg);
+      }
+
+      &:active {
+        transform: rotate(-360deg);
+      }
+    }
+
+    img {
+      aspect-ratio: 2/1;
+      object-fit: cover;
+      width: 100%;
+    }
   }
 }
 </style>
