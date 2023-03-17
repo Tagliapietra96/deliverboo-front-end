c278b0eec87a98576b4c36df3ba3a6f860604fd7
PaoloWD <paolo.lore.93@gmail.com>
2023-03-17 01:20:02 +0100

    add cart icon and total price on cart banner

--- src/components/dishes/DishesList.vue
+++ src/components/dishes/DishesList.vue
@@ -1,18 +1,36 @@
 <template>
   <Loader v-if="store.dt.loading"></Loader>
   <div v-else class="my-container">
-    <div v-if="store.dt.myChart.length > 0 && !forcedExit" :class="popUpVisibility ? 'active' : ''"
-      class="card my-pop-up shadow overflow-hidden p-3">
-      <button @click="deletePop()" class="delete btn-sm btn btn-primary btn-custom">
+    <div
+      v-if="store.dt.myChart.length > 0 && !forcedExit"
+      :class="popUpVisibility ? 'active' : ''"
+      class="card my-pop-up shadow overflow-hidden p-3"
+    >
+      <button
+        @click="deletePop()"
+        class="delete btn-sm btn btn-primary btn-custom"
+      >
         <i class="fa-solid fa-trash-can"></i>
       </button>
       <div>
         <div class="row">
           <div class="col-12 col-md-6 col-lg-5 col-xl-4">
-            <img v-if="store.dt.myChart[shoppingIndex].item.image.includes('http')"
-              :src="store.dt.myChart[shoppingIndex].item.image" class="img-fluid" alt="" />
-            <img v-else :src="store.dt.beUrl + '/storage/' + store.dt.myChart[shoppingIndex].item.image" class="img-fluid"
-              alt="" />
+            <img
+              v-if="store.dt.myChart[shoppingIndex].item.image.includes('http')"
+              :src="store.dt.myChart[shoppingIndex].item.image"
+              class="img-fluid"
+              alt=""
+            />
+            <img
+              v-else
+              :src="
+                store.dt.beUrl +
+                '/storage/' +
+                store.dt.myChart[shoppingIndex].item.image
+              "
+              class="img-fluid"
+              alt=""
+            />
           </div>
           <div class="col-12 col-md-6 col-lg-7 col-xl-8">
             <h5 class="d-none d-xxl-block">
@@ -20,34 +38,69 @@
               {{ store.dt.myChart[shoppingIndex].item.name }}!
             </h5>
             <div class="d-none d-md-flex justify-content-around pt-3">
-              <button class="btn btn-primary btn-custom btn-sm px-3" @click="minusPop()">
+              <button
+                class="btn btn-primary btn-custom btn-sm px-3"
+                @click="minusPop()"
+              >
                 -
               </button>
               <div class="card w-25 text-center py-2 d-none d-lg-block">
                 {{ store.dt.myChart[shoppingIndex].quantity }}
               </div>
-              <button class="btn btn-primary btn-custom btn-sm px-3" @click="plusPop()">
+              <button
+                class="btn btn-primary btn-custom btn-sm px-3"
+                @click="plusPop()"
+              >
                 +
               </button>
             </div>
             <div class="pt-3">
-              <router-link :to="'/cart'" class="custom-color" @click="toChartPage()"><small>Vai al tuo
-                  carrello!</small></router-link>
+              <router-link
+                :to="'/cart'"
+                class="custom-color"
+                @click="toChartPage()"
+                ><small>Vai al tuo carrello!</small></router-link
+              >
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div v-if="cardShow.active" :class="cardShow.active ? 'active' : ''" class="selected-dish">
+    <div
+      v-if="cardShow.active"
+      :class="cardShow.active ? 'active' : ''"
+      class="selected-dish"
+    >
       <div class="h-100 w-100 bg-invisible">
         <div class="card card-show border-0 rounded-4 overflow-hidden">
-          <button @click="exitShow()" class="btn btn-primary btn-custom close-show">
+          <button
+            @click="exitShow()"
+            class="btn btn-primary btn-custom close-show"
+          >
             <i class="fa-solid fa-xmark"></i>
           </button>
-          <img v-if="cardShow.card.image.includes('http')" :src="cardShow.card.image" class="card-img-top" alt="..."
-            :style="(cardShow.card.visibility) ? 'filter: grayscale(0)' : 'filter: grayscale(100%)'" />
-          <img v-else :src="store.dt.beUrl + '/storage/' + cardShow.card.image" class="card-img-top" alt="..."
-            :style="(cardShow.card.visibility) ? 'filter: grayscale(0)' : 'filter: grayscale(100%)'" />
+          <img
+            v-if="cardShow.card.image.includes('http')"
+            :src="cardShow.card.image"
+            class="card-img-top"
+            alt="..."
+            :style="
+              cardShow.card.visibility
+                ? 'filter: grayscale(0)'
+                : 'filter: grayscale(100%)'
+            "
+          />
+          <img
+            v-else
+            :src="store.dt.beUrl + '/storage/' + cardShow.card.image"
+            class="card-img-top"
+            alt="..."
+            :style="
+              cardShow.card.visibility
+                ? 'filter: grayscale(0)'
+                : 'filter: grayscale(100%)'
+            "
+          />
           <div class="card-body">
             <h5 class="card-title">{{ cardShow.card.name }}</h5>
             <p class="card-text">{{ cardShow.card.description }}</p>
@@ -56,27 +109,61 @@
                 € {{ totalPrice(cardShow.card.price, cardShow.quantity) }}
               </div>
               <div class="text-center py-3" v-if="cardShow.card.visibility">
-                <div class="btn-group " role="group">
-                  <button type="button"
-                    :class="(store.dt.myChart.length === 0) ? '' : (store.dt.myChart[0].item.restaurant_id !== store.dt.selectedRestaurant) ? 'disabled' : ''"
-                    class="btn btn-primary btn-custom d-none d-sm-inline" @click="minusBtn()">
+                <div class="btn-group" role="group">
+                  <button
+                    type="button"
+                    :class="
+                      store.dt.myChart.length === 0
+                        ? ''
+                        : store.dt.myChart[0].item.restaurant_id !==
+                          store.dt.selectedRestaurant
+                        ? 'disabled'
+                        : ''
+                    "
+                    class="btn btn-primary btn-custom d-none d-sm-inline"
+                    @click="minusBtn()"
+                  >
                     -
                   </button>
-                  <button type="button"
-                    :class="(store.dt.myChart.length === 0) ? '' : (store.dt.myChart[0].item.restaurant_id !== store.dt.selectedRestaurant) ? 'disabled' : ''"
-                    class="btn btn-primary btn-custom" @click="chartBtn()">
-                    <i class="fa-solid fa-cart-shopping me-3 d-none d-md-inline"></i>
+                  <button
+                    type="button"
+                    :class="
+                      store.dt.myChart.length === 0
+                        ? ''
+                        : store.dt.myChart[0].item.restaurant_id !==
+                          store.dt.selectedRestaurant
+                        ? 'disabled'
+                        : ''
+                    "
+                    class="btn btn-primary btn-custom"
+                    @click="chartBtn()"
+                  >
+                    <i
+                      class="fa-solid fa-cart-shopping me-3 d-none d-md-inline"
+                    ></i>
                     {{ cardShow.quantity }}
                   </button>
-                  <button type="button"
-                    :class="(store.dt.myChart.length === 0) ? '' : (store.dt.myChart[0].item.restaurant_id !== store.dt.selectedRestaurant) ? 'disabled' : ''"
-                    class="btn btn-primary btn-custom d-none d-sm-inline" @click="plusBtn()">
+                  <button
+                    type="button"
+                    :class="
+                      store.dt.myChart.length === 0
+                        ? ''
+                        : store.dt.myChart[0].item.restaurant_id !==
+                          store.dt.selectedRestaurant
+                        ? 'disabled'
+                        : ''
+                    "
+                    class="btn btn-primary btn-custom d-none d-sm-inline"
+                    @click="plusBtn()"
+                  >
                     +
                   </button>
                 </div>
               </div>
               <div class="pt-3" v-else>
-                <h5 class="text-center custom-color fw-bold">Prodotto attualmente non disponibile</h5>
+                <h5 class="text-center custom-color fw-bold">
+                  Prodotto attualmente non disponibile
+                </h5>
               </div>
             </div>
           </div>
@@ -84,48 +171,91 @@
       </div>
     </div>
 
-    
-    <div :class="(showCart) ? 'active' : ''" class="cart-preview shadow" v-if="(store.dt.myChart.length > 0 && store.dt.myChart[0].item.restaurant_id === store.dt.selectedRestaurant)">
-      <div class="open-close" @click="(showCart) ? showCart = false : showCart = true ">
-        <i class="fa-solid fa-chevron-up "></i>
+    <div
+      :class="showCart ? 'active' : ''"
+      class="cart-preview shadow"
+      v-if="
+        store.dt.myChart.length > 0 &&
+        store.dt.myChart[0].item.restaurant_id === store.dt.selectedRestaurant
+      "
+    >
+      <div
+        class="open-close"
+        @click="showCart ? (showCart = false) : (showCart = true)"
+      >
+        <i class="fa-solid fa-cart-shopping me-2 ms-2"></i>
       </div>
       <h5 class="py-3 custom-color ps-3">Il tuo carrello</h5>
       <div class="container-fluid overflow-auto">
-        <div class="row align-items-center  border-bottom py-2" v-for="(dish, i) in store.dt.myChart">
-        <div class="col-10 fw-bold">
-          {{ dish.item.name }}:
+        <div
+          class="row align-items-center border-bottom py-2"
+          v-for="(dish, i) in store.dt.myChart"
+        >
+          <div class="col-10 fw-bold">{{ dish.item.name }}:</div>
+          <div class="col-2">
+            {{ dish.quantity }}
+          </div>
         </div>
-        <div class="col-2">
-          {{ dish.quantity }}
+        <div class="d-flex justify-content-between fw-bold">
+          <div>Totale</div>
+          <div>{{ total_order.toFixed(2) }} €</div>
         </div>
       </div>
-      </div>
-      
     </div>
 
     <div class="container pb-5">
       <h3 class="py-5">IL NOSTRO MENU</h3>
 
-      <div v-if="(store.dt.myChart.length > 0 && store.dt.myChart[0].item.restaurant_id !== store.dt.selectedRestaurant)"
-        class="alert alert-info custom-color fw-bolder py-4 fs-4">
-        Stai provando ad acquistare un prodotto di un ristorante diverso da quello al quale stai già acquistando. Sei
-        pregato di selezionare piatti da un ristorante alla volta! <br>
-        Se vuoi comunque acquistare questi prodotti, svuota prima il carrello <br>
+      <div
+        v-if="
+          store.dt.myChart.length > 0 &&
+          store.dt.myChart[0].item.restaurant_id !== store.dt.selectedRestaurant
+        "
+        class="alert alert-info custom-color fw-bolder py-4 fs-4"
+      >
+        Stai provando ad acquistare un prodotto di un ristorante diverso da
+        quello al quale stai già acquistando. Sei pregato di selezionare piatti
+        da un ristorante alla volta! <br />
+        Se vuoi comunque acquistare questi prodotti, svuota prima il carrello
+        <br />
         <!-- @click="store.dt.myChart = []; store.fn.saveStorage(); store.fn.loadStorage()" -->
-         <button v-if="store.dt.myChart.length > 0"
+        <button
+          v-if="store.dt.myChart.length > 0"
           class="btn btn-ptimary btn-custom mt-3"
-          @click="deleteChart()" >
+          @click="deleteChart()"
+        >
           Elimina carrello
         </button>
       </div>
-      <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3 row-cols-xxl-4 g-4 mt-3">
+      <div
+        class="row row-cols-1 row-cols-lg-2 row-cols-xl-3 row-cols-xxl-4 g-4 mt-3"
+      >
         <div class="col" v-for="dish in store.dt.dishesList" :key="dish.id">
-          <div class="card card-index h-100 mb-3 shadow overflow-hidden" @click="onCardClick(dish)">
+          <div
+            class="card card-index h-100 mb-3 shadow overflow-hidden"
+            @click="onCardClick(dish)"
+          >
             <div class="row g-0 h-100">
-              <div class="col-4 h-100" :style="(dish.visibility) ? 'filter: grayscale(0)' : 'filter: grayscale(100%)'">
-                <img v-if="dish.image.includes('http')" class="img-fluid rounded-start h-100" :src="dish.image" alt="" />
-                <img v-else class="img-fluid rounded-start h-100" :src="store.dt.beUrl + '/storage/' + dish.image"
-                  alt="" />
+              <div
+                class="col-4 h-100"
+                :style="
+                  dish.visibility
+                    ? 'filter: grayscale(0)'
+                    : 'filter: grayscale(100%)'
+                "
+              >
+                <img
+                  v-if="dish.image.includes('http')"
+                  class="img-fluid rounded-start h-100"
+                  :src="dish.image"
+                  alt=""
+                />
+                <img
+                  v-else
+                  class="img-fluid rounded-start h-100"
+                  :src="store.dt.beUrl + '/storage/' + dish.image"
+                  alt=""
+                />
               </div>
               <div class="col-8 h-100">
                 <div class="card-body h-100 d-flex flex-column">
@@ -133,7 +263,9 @@
                   <p class="card-text">{{ dish.description }}</p>
                   <div class="flex-fill"></div>
                   <p class="card-text">
-                    <small class="text-muted">Prezzo:€ {{ dish.price.toFixed(2) }}</small>
+                    <small class="text-muted"
+                      >Prezzo:€ {{ dish.price.toFixed(2) }}</small
+                    >
                   </p>
                 </div>
               </div>
@@ -212,7 +344,7 @@ export default {
           this.cardShow.card.price,
           this.cardShow.quantity
         ),
-      }
+      };
 
       let toReturn = true;
       let abort = false;
@@ -231,16 +363,16 @@ export default {
             );
             this.shoppingIndex = i;
           }
-        })
+        });
       }
 
       if (toReturn) {
         store.dt.myChart.push(toPush);
-        this.shoppingIndex = (store.dt.myChart.length - 1);
+        this.shoppingIndex = store.dt.myChart.length - 1;
       }
 
       if (abort) {
-        return
+        return;
       } else {
         this.exitShow();
         this.forcedExit = false;
@@ -264,7 +396,7 @@ export default {
       store.dt.myChart[this.shoppingIndex].price = this.totalPrice(
         store.dt.myChart[this.shoppingIndex].item.price,
         store.dt.myChart[this.shoppingIndex].quantity
-      )
+      );
     },
     plusPop() {
       if (store.dt.myChart[this.shoppingIndex].quantity === 99) {
@@ -276,7 +408,7 @@ export default {
       store.dt.myChart[this.shoppingIndex].price = this.totalPrice(
         store.dt.myChart[this.shoppingIndex].item.price,
         store.dt.myChart[this.shoppingIndex].quantity
-      )
+      );
     },
     deletePop() {
       this.popUpVisibility = false;
@@ -284,7 +416,7 @@ export default {
       store.dt.myChart.splice(this.shoppingIndex, 1);
     },
     toChartPage() {
-      store.dt.headerLinks.forEach(element => {
+      store.dt.headerLinks.forEach((element) => {
         element.active = false;
       });
       store.dt.headerLinks[2].active = true;
@@ -295,14 +427,14 @@ export default {
         this.store.fn.saveStorage();
         this.store.fn.loadStorage();
       }
-    }
+    },
   },
   mounted() {
     store.fn.fetchDishes();
     store.fn.loadStorage();
   },
   beforeUnmount() {
-    store.fn.saveStorage()
+    store.fn.saveStorage();
   },
 };
 </script>
@@ -428,7 +560,7 @@ export default {
   min-height: calc(100vh - 214px);
 }
 
-.cart-preview{
+.cart-preview {
   position: fixed;
   right: 0;
   top: 74px;
@@ -437,17 +569,15 @@ export default {
   background-color: white;
   z-index: 3;
 
-  transition: all .4s;
+  transition: all 0.4s;
 
   h5,
-  .container-fluid{
+  .container-fluid {
     opacity: 0;
-
   }
 
-
-  .open-close{
-    padding: .7rem 2rem;
+  .open-close {
+    padding: 0.7rem 2rem;
     background-color: white;
     position: absolute;
     top: 50%;
@@ -456,32 +586,30 @@ export default {
     border-radius: 10px;
     border-top: 1px solid lightgrey;
     padding-bottom: 0.3rem;
-    transition: right .4s;
+    transition: right 0.4s;
     cursor: pointer;
 
-    i{
+    i {
       transform: rotate(0);
-      transition: all .7s;
+      transition: all 0.7s;
     }
   }
 
-  &.active{
+  &.active {
     left: calc(100vw - 250px);
 
-
     h5,
-  .container-fluid{
-    opacity: 1;
-    transition: opacity .4s .2s;
-  }
-    .open-close{
+    .container-fluid {
+      opacity: 1;
+      transition: opacity 0.4s 0.2s;
+    }
+    .open-close {
       right: 210px;
 
-      i{
+      i {
         transform: rotate(-180deg);
       }
     }
   }
 }
-
 </style>
