dea34a99014e330deb547f5f0cf61d55c0c0d759
Tagliapietra96 <tagliatek96@gmail.com>
2023-03-19 12:00:45 +0100

    working on store system

---
+++ src/stores/data/dt.js
@@ -0,0 +1,12 @@
+import { reactive } from 'vue';
+import { bool } from './bool';
+import { arr } from './arr';
+import { str } from './str';
+
+
+export const dt = reactive({
+    bool,
+    arr,
+    str,
+    
+}) 
