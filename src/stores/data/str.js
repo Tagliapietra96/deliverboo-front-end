0ec4d473557c5e4466bcdb37c3dd7686e18d3fe6
Tagliapietra96 <tagliatek96@gmail.com>
2023-03-19 12:01:22 +0100

    add new store components

---
+++ src/stores/data/str.js
@@ -0,0 +1,14 @@
+import { reactive } from 'vue';
+
+
+export const str = reactive({
+    paymentUrl:
+        "http://127.0.0.1:8000/api/orders/make/payment?token=fake-valid-nonce",
+    beUrl: "http://127.0.0.1:8000",
+    categoriesUrl: "/api/categories",
+    restaurantsUrl: "/api/restaurants/index",
+    restaurantFiltersUrl: "/api/restaurants",
+    dishesUrl: "/api/dishes/",
+    dishShowUrl: "/api/dishes/show/",
+    restaurantsMessage: "",
+}) 
