4002598d6a3567fdb9593762b1333b0952e79335
Tagliapietra96 <tagliatek96@gmail.com>
2023-03-19 12:34:05 +0100

    created num data components

--- src/stores/data/dt.js
+++ src/stores/data/dt.js
@@ -2,11 +2,12 @@ import { reactive } from 'vue';
 import { bool } from './bool';
 import { arr } from './arr';
 import { str } from './str';
+import { num } from './num';
 
 
 export const dt = reactive({
     bool,
     arr,
-    str,
-    
+    num,
+    str
 }) 
---
+++ src/stores/data/num.js
@@ -0,0 +1,6 @@
+import { reactive } from 'vue';
+
+export const num = reactive({
+    selectedRestaurant: Number,
+    quantityDish: 1,
+});
