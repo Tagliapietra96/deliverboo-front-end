b204b1415de70baf3b965c3f2eac62291444ca3c
Tagliapietra96 <tagliatek96@gmail.com>
2023-03-19 13:28:36 +0100

    add ajax components to fn-store

---
+++ src/stores/functions/ajax.js
@@ -0,0 +1,11 @@
+import {reactive} from 'vue';
+import {api } from '../api/api';
+
+export const ajax = reactive({
+    fetchCategories(){
+        api.categories.index();
+    },
+    fetchRestaurants(){
+        api.restaurants.index(category);
+    }
+})
--- src/stores/main-store.js
+++ src/stores/main-store.js
@@ -1,6 +1,8 @@
-import {reactive} from 'vue';
-import {dt} from './data/dt'
+import { reactive } from 'vue';
+import { dt } from './data/dt'
+import { fn } from './data/fn';
 
 export const store = reactive({
-    dt
+    dt,
+    fn
 })
