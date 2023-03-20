113815ef349ef51d5e231da93a70899b5d565353
LuigginaRossi <luigginavaldivia@gmail.com>
2023-03-20 12:02:24 +0100

    fixed footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,16 +1,16 @@
 <template lang="">
-    <footer class="bg-dark pb-3 pt-5">
+    <footer class="bg-dark pb-3 pt-5 ">
         <div class="container pb-3">
             <div class="row justify-content-between align-items-center">
-                <div class="col-4">
-                    <router-link class="text-decoration-none" :to="'/'" @click="onLogoClick()">
-                        <div class= "fs-1 fw-bold d-flex align-items-center text-light">
+                <div class="col-md-4 col-sm-8  ">
+                    <router-link class="text-decoration-none " :to="'/'" @click="onLogoClick()">
+                        <div class= "fs-1 fw-bold d-flex align-items-center text-light mb-5">
                             <i class="fa-solid fa-bowl-food px-2"></i>
                             DeliveBoo
                         </div>
                     </router-link>
                 </div>
-                <div class="col-4">
+                <div class="col-md-4 col-sm-8 ">
                     <div class="text-white"><span class="fw-semibold">Boolean Careers </span>&#8226; Classe #79 &#8226; <i><span class="fw-bold">DreamTeam6</span></i> <br> <small>Copyright 2022/2023 &#169; Privacy policy &#174;</small></div>
                 </div>
             </div>
